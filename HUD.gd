7308f7000d3c8ccd2057c9544f1d74b8cc2b6898
Jayden Ivanovic <jayden.ivanovic@gmail.com>
2023-02-18 18:45:26 +1100

    Add game over state and restart feature

--- HUD.gd
+++ HUD.gd
@@ -3,12 +3,25 @@ extends Panel
 func _ready():
 	var player = get_node("../Player");
 	player.connect("player_health_reduced", self, "on_health_reduced")
-	player.connect("dangerous_tile_changed", self, "on_dangerous_tile_changed")
+	player.connect("active_tile_changed", self, "on_active_tile_changed")
+	var restart_button = get_node("../GameOverMessage/Button")
+	restart_button.connect("pressed", self, "restart_game")
 
 func on_health_reduced(health):
 	var label = get_node("Health")
 	label.text = String(round(health)) + "%"
+	if health <= 0:
+		show_game_over_screen()
 
-func on_dangerous_tile_changed(tile_name):
-	var label = get_node("DangerousTile")
+func on_active_tile_changed(tile_name):
+	var label = get_node("ActiveTile")
 	label.text = tile_name
+
+func show_game_over_screen():
+	get_tree().paused = true
+	var game_over_message = get_node("../GameOverMessage")
+	game_over_message.show()
+
+func restart_game():
+	get_tree().paused = false
+	get_tree().reload_current_scene()
--- Player.gd
+++ Player.gd
@@ -6,10 +6,10 @@ export var environment_damage_decay = 10
 
 var tile_map: TileMap
 var random_env_damage_timer: Timer
-var dangerous_tile = 0.0
+var active_tile = 0
 
 signal player_health_reduced
-signal dangerous_tile_changed
+signal active_tile_changed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,12 +17,17 @@ func _ready():
 	tile_map = get_node("../TileMap")
 	random_env_damage_timer = get_node("../RandomEnvDamageTimer")
 	random_env_damage_timer.connect("timeout", self, 'on_random_env_damage_timeout')
+	initialize_state()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	process_movement(delta)
 	process_environment_damage(delta)
 	
+func initialize_state():
+	active_tile = round(rand_range(0,3))
+	emit_signal("active_tile_changed", get_tile_name(active_tile))
+
 func process_movement(delta):
 	if Input.is_action_pressed("move_left"):
 		position += Vector2.LEFT * speed * delta
@@ -37,14 +42,14 @@ func process_movement(delta):
 
 func process_environment_damage(delta):
 	var tile = tile_map.get_cellv(tile_map.world_to_map(position))
-	if tile == dangerous_tile:
+	if tile != active_tile:
 		health -=  environment_damage_decay * delta
 		emit_signal("player_health_reduced", health)
 
 func on_random_env_damage_timeout():
-	dangerous_tile = round(rand_range(0,3))
-	emit_signal("dangerous_tile_changed", get_tile_name(dangerous_tile))
-	random_env_damage_timer.start(rand_range(1,4))
+	active_tile = round(rand_range(0,3))
+	emit_signal("active_tile_changed", get_tile_name(active_tile))
+	random_env_damage_timer.start(rand_range(2,4))
 
 func get_tile_name(tile_index):
 	return ["Yellow", "Red", "Blue", "Green"][tile_index]
