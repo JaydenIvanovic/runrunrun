6cf256208fb4d71f0b7bb9df3a6c733356f990ff
Jayden Ivanovic <jayden.ivanovic@gmail.com>
2023-04-08 16:55:58 +1000

    Upgrade to Godot 4.x.x

--- HUD.gd
+++ HUD.gd
@@ -1,27 +1,24 @@
 extends Panel
 
 func _ready():
-	var player = get_node("../Player");
-	player.connect("player_health_reduced", self, "on_health_reduced")
-	player.connect("active_tile_changed", self, "on_active_tile_changed")
-	var restart_button = get_node("../GameOverMessage/Button")
-	restart_button.connect("pressed", self, "restart_game")
-
-func on_health_reduced(health):
-	var label = get_node("Health")
-	label.text = String(round(health)) + "%"
-	if health <= 0:
-		show_game_over_screen()
-
-func on_active_tile_changed(tile_name):
-	var label = get_node("ActiveTile")
-	label.text = tile_name
-
-func show_game_over_screen():
-	get_tree().paused = true
-	var game_over_message = get_node("../GameOverMessage")
-	game_over_message.show()
-
-func restart_game():
-	get_tree().paused = false
-	get_tree().reload_current_scene()
+	var player = $"../Player"
+	
+	player.connect("player_health_changed", func(health):
+		var label = $Health
+		label.text = str(round(health)) + "%"
+		if health <= 0:
+			get_tree().paused = true
+			var game_over_message = $"../GameOverMessage"
+			game_over_message.show()
+	)
+	
+	player.connect("active_tile_changed", func(tile_name):
+		var label = $ActiveTile
+		label.text = tile_name
+	)
+	
+	var restart_button = $"../GameOverMessage/Button"
+	restart_button.connect("pressed", func():
+		get_tree().paused = false
+		get_tree().reload_current_scene()
+	)
--- Player.gd
+++ Player.gd
@@ -1,23 +1,24 @@
-extends Sprite
+extends Sprite2D
 
-export var health = 100
-export var speed = 200
-export var environment_damage_decay = 10
+@export var health = 100
+@export var speed = 200
+@export var environment_damage_decay = 10
 
 var tile_map: TileMap
 var random_env_damage_timer: Timer
 var active_tile = 0
+var tile_being_stood_on = 0
 var is_moving = false
 
-signal player_health_reduced
+signal player_health_changed
 signal active_tile_changed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	tile_map = get_node("../TileMap")
-	random_env_damage_timer = get_node("../RandomEnvDamageTimer")
-	random_env_damage_timer.connect("timeout", self, 'on_random_env_damage_timeout')
+	tile_map = $"../TileMap"
+	random_env_damage_timer = $RandomEnvDamageTimer
+	random_env_damage_timer.connect("timeout", randomize_active_tile)
 	initialize_state()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,7 +27,7 @@ func _process(delta):
 	process_environment_damage(delta)
 	
 func initialize_state():
-	active_tile = round(rand_range(0,3))
+	active_tile = round(randf_range(0,3))
 	set_color(get_tile_name(active_tile))
 	emit_signal("active_tile_changed", get_tile_name(active_tile))
 
@@ -54,17 +55,17 @@ func process_movement(delta):
 		$AnimationPlayer.stop(true)
 
 func process_environment_damage(delta):
-	var tile = tile_map.get_cellv(tile_map.world_to_map(position))
+	var tile = tile_map.get_cell_source_id(0, tile_map.local_to_map(position))
 	if tile != active_tile:
 		health -=  environment_damage_decay * delta
-		emit_signal("player_health_reduced", health)
+		emit_signal("player_health_changed", health)
 
-func on_random_env_damage_timeout():
-	active_tile = round(rand_range(0,3))
+func randomize_active_tile():
+	active_tile = round(randf_range(0,3))
 	var active_tile_name = get_tile_name(active_tile)
 	emit_signal("active_tile_changed", active_tile_name)
 	set_color(active_tile_name)
-	random_env_damage_timer.start(rand_range(2,4))
+	random_env_damage_timer.start(randf_range(2,4))
 
 func get_tile_name(tile_index):
 	return ["Yellow", "Red", "Blue", "Green"][tile_index]
@@ -72,22 +73,22 @@ func get_tile_name(tile_index):
 func set_color(tile_name):
 	match tile_name:
 		"Yellow": 
-			material.set_shader_param("red", 0.85)
-			material.set_shader_param("green", 0.55)
-			material.set_shader_param("blue", 0.35)
+			material.set_shader_parameter("red", 0.85)
+			material.set_shader_parameter("green", 0.55)
+			material.set_shader_parameter("blue", 0.35)
 		"Red":
-			material.set_shader_param("red", 1.0)
-			material.set_shader_param("green", 0.3)
-			material.set_shader_param("blue", 0.9)
+			material.set_shader_parameter("red", 1.0)
+			material.set_shader_parameter("green", 0.3)
+			material.set_shader_parameter("blue", 0.9)
 		"Blue":
-			material.set_shader_param("red", 0.0)
-			material.set_shader_param("green", 0.0)
-			material.set_shader_param("blue", 1.0)
+			material.set_shader_parameter("red", 0.0)
+			material.set_shader_parameter("green", 0.0)
+			material.set_shader_parameter("blue", 1.0)
 		"Green":
-			material.set_shader_param("red", 0.0)
-			material.set_shader_param("green", 0.7)
-			material.set_shader_param("blue", 0.3)
+			material.set_shader_parameter("red", 0.0)
+			material.set_shader_parameter("green", 0.7)
+			material.set_shader_parameter("blue", 0.3)
 		_:
-			material.set_shader_param("red", 0.0)
-			material.set_shader_param("green", 0.0)
-			material.set_shader_param("blue", 0.0)
+			material.set_shader_parameter("red", 0.0)
+			material.set_shader_parameter("green", 0.0)
+			material.set_shader_parameter("blue", 0.0)
